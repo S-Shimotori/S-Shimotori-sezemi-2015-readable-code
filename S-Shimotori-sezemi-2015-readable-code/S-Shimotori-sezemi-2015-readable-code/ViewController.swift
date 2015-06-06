6895c5da02a9411509f2b18bd89d1eba4f933089
Shigure Shimotori <S-Shimotori@S-Shimotoris-MacBook-Pro.local>
2015-06-06 11:55:21 +0900

    spec3: add ios application file

---
+++ S-Shimotori-sezemi-2015-readable-code/S-Shimotori-sezemi-2015-readable-code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S-Shimotori-sezemi-2015-readable-code
+//
+//  Created by S-Shimotori on 6/6/15.
+//  Copyright (c) 2015 S-Shimotori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
